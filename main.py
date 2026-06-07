01451a1eaddd3054addf10f481ae73ed7a6eeb1c
Muhammad Talha <bsdsf25m009@pucit.edu.pk>
2026-06-08 00:05:00 +0500

    Update request header and implement retry mechanism
    Refactor header definition and add retry logic for requests.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import csv
 import requests
 from bs4 import BeautifulSoup
@@ -10,21 +11,19 @@ load_dotenv()
 MY_EMAIL=os.getenv("MY_EMAIL")
 PASSWORD=os.getenv("PASSWORD")
 DESIRED_PRICE=20000
-header = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "en-GB,de;q=0.8,fr;q=0.6,en;q=0.4,ja;q=0.2",
-    "Dnt": "1",
-    "Priority": "u=1",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-User": "?1",
-    "Sec-Gpc": "1",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
+header = header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
 }
-response=requests.get(URL,headers=header)
+response
+for i in range(3):
+    response = requests.get(URL, headers=header)
+    if "captcha" not in response.text.lower():
+        print("Status Code:", response.status_code)
+        print("Final URL:", response.url)
+        print(response.text[:1000])
+        break
+    time.sleep(5)
 soup=BeautifulSoup(response.text,"html.parser")
 price_tag = soup.find(class_="a-offscreen")
 
